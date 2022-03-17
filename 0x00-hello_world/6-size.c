56088f1826caa717db39b181dd78686bfe2a09e2
Husseinmoha <husseinmoha0722@gmail.com>
2022-03-17 14:55:29 +0300

    My sixth task commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a char: %d byte(s)\n", sizeof(char));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dan int[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[int[[C[C[C[[C[C[C));
+printf("Size of a char: %d byte(s)\n", sizeof(char));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D long int: %d byte(s)\n", sizeof(long int));[3;
+printf("Size of a char: %d byte(s)\n", sizeof(char));[D[long long int));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[Clong long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
