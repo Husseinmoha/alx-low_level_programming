dfdfaa9f3bd0e9dfe6c5ccf7e8ef23eab3511eca
Husseinmoha <husseinmoha0722@gmail.com>
2022-03-19 12:29:26 +0300

    My first task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include <main.h>
+
 /**
- * print_alphabet - Prints alphabet in lowercase
+ * print_alphabet - Prints alphabet in lowercase.
  *
+ * Return: Always 0 to (Success)
  **/
 void print_alphabet(void)
 {
-	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
 	_putchar('\n');
 }
